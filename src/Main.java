fd6a2c5915f7e386728f52b83cd3db421625ae64
Guy Miron <76874773+LOLMASTER420@users.noreply.github.com>
2023-09-10 09:08:17 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,15 @@
-aaaaaaaaaaaaaaaaaaaaa
+public class test1 {
+	
+	public static int azeret(int n) {
+		if (n==1) return 1;
+		return n+azeret(n-1);
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		 
+
+	}
+
+}
